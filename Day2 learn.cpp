37ef19242c899cffa04feb827a03094e92a8f563
KULDEEP <kkuldeep4455@gmail.com>
2026-08-29 23:57:29 +0530

    Create Day2 learn.cpp

---
+++ Day2 learn.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Day 2 - Variables and Data Types
+    int age = 19;
+    string name = "First Year IT";
+    float cgpa = 8.5;
+    char grade = 'A';
+    
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "CGPA: " << cgpa << endl;
+    cout << "Grade: " << grade << endl;
+    
+    int a = 10, b = 20;
+    cout << "Sum: " << a + b << endl;
+    
+    return 0;
+}
