3d0a88e2580841ce4b78aab8027d8bccfad71c7c
KULDEEP <kkuldeep4455@gmail.com>
2026-08-31 20:29:47 +0530

    Day3.cpp: (input/output)

--- Day3.cpp
+++ Day3.cpp
@@ -26,12 +26,12 @@ int main (){
     
     cout<< "sum of the num1 or num2:\n" 
     <<num1+num2 << "\n";
-    
+
     
     cout<< "product of the num1 or num2: \n"<<num1*num2;
     
 /* cout mtlb hota h print krna or  result dena 
-    
+
     
 cin mtlb hota h ki user se pucha jaye
 Ex-: yha hum chahte h ki user se num1 or num2 pucha jaye isliye humne cin use kiya */
