ea805b11167815efc87a13ac9c317a1618d90a23
KULDEEP <kkuldeep4455@gmail.com>
2026-09-01 17:29:37 +0530

    Day4.cpp
    How to use (constant ) in cpp.

---
+++ Day4.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main () {
+    
+    int a = 4;
+    cout<<"the value of a was: "<<a<<endl;
+    
+    a = 9;
+    
+    
+    cout<<"the value of a is : "<<a<<endl;
+    
+    
+  // kisi variable ki value change nhi krni ho to constant use hota h // 
+    
+    
+    //////////////////constant in c++//////////////////
+   
+   const int A = 10;
+   //A = 100;
+   
+   cout<<"the value of A : "<<A;
+   
+    
+    
+    
+    return 0;
+    
+    
+}
+    
+    
